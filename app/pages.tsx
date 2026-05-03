788e4cf12adb2ace930b8c7ab192bf8a32ff6378
tatianacsogor-stack <tatianacsogor@gmail.com>
2026-05-03 16:01:05 -0400

    Add files via upload

---
+++ app/pages.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://tati-finance-app-ovfkztjgfrjhfrayfyj5id.streamlit.app"
+        style={{ width: "100%", height: "100%", border: "none" }}
+      />
+    </div>
+  );
+}
